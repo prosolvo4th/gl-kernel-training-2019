6f9db9fba0448894acb274a771109a7ebf3c3dd2
yuriy.podgorniy <yuriy.pidgorniy@gmail.com>
2019-10-04 20:06:40 +0300

    02-Git: Add simple reading symbol
    Signed-off-by: yuriy.podgorniy <yuriy.pidgorniy@gmail.com>

--- 02-Git/RockPaperScisors.cpp
+++ 02-Git/RockPaperScisors.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv) {
-	cout << "Hello world";
-	return 0;
+enum
+{
+    Rock = 0, Paper, Scisors,
+};
+
+char pick[3] =
+{
+    'r',
+    'p',
+    's' };
+
+int main(int argc, char **argv)
+{
+    char symb = ' ';
+    int userPick = -1;
+
+    cout << "Please choose: rock (r) - paper (p) - scissors (s)" << endl;
+    cin >> symb;
+
+    cout << "You choose " << pick[userPick] << endl;
+
+    return 0;
 }
 
