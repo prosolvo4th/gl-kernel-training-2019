d646bcfedc69ca3d48d69112ee1c20025d4ea409
yuriy.podgorniy <yuriy.pidgorniy@gmail.com>
2019-10-12 15:56:31 +0300

    add first version of script
    using temporary file for storing list of files
    using temporary files for storing md5 sum
    using wc for getting file size
    add clean up mechanism
    
    Signed-off-by: yuriy.podgorniy <yuriy.pidgorniy@gmail.com>

---
+++ 03-Bash/find_dup_files/find_dup_files.sh
@@ -0,0 +1,62 @@
+#!/bin/bash +x
+
+# set default dir
+sourceDir=$(pwd)
+
+# set new source dir if available
+if [ $# -ne 0 ]; then 
+    sourceDir=$1
+fi
+
+cd $sourceDir
+# create list of files in dir
+find . * -maxdepth 0 -type f > fileList
+
+# check if file list created
+if [ ! -e fileList ]; then
+    echo cant create fileList
+    exit 1
+fi
+
+#create file with checksum for each file
+cat fileList | while read line
+do
+    tmp=$(md5sum "$line") 
+    echo $tmp | cut -d ' ' -f1 > "$line".md5sum
+done
+
+# compare contents of *.md5sum files
+cat fileList | while read md5File1
+do
+    declare -i sameFile
+    sameFile=0
+    cat fileList | while read md5File2
+    do
+        # ignore the same file
+        if [ $sameFile -eq 0 ]; then
+            if [ "$md5File1" != "$md5File2" ]; then
+                # search the filename in fileList
+                continue
+            else
+                # finded the same file in fileList
+                sameFile=1
+                continue
+            fi
+        fi
+    
+        if [ $(cat "$md5File1".md5sum) = $(cat "$md5File2".md5sum) ]; then
+            tmp=$(wc -c $md5File1)
+            file1Size=$(echo $tmp | cut -d ' ' -f1)
+            tmp=$(wc -c $md5File2)
+            file2Size=$(echo $tmp | cut -d ' ' -f1)
+            echo "sum eq : $md5File1 - $file1Size | $md5File2 - $file2Size"
+        fi
+    done
+done
+
+# clean up
+cat fileList | while read md5File
+do
+    rm "$md5File".md5sum
+done
+rm fileList
